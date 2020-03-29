86ed83816abeea9505eb6da70c615f9b496b5ec9
Eszter <schweszter@gmail.com>
2020-03-29 14:00:08 +0200

    adding buttons

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,12 +5,32 @@ void main() => (FirstApp());
 class FirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var question = [
+      'What is your favorie quarantine song?',
+      'What is your favorite taste?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my first text!'),
+        body: Column(
+          children: [
+            Text('The question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
